5d07c162accc55936795de39c8807359076a65cf
Ligh7ninG <Lt1118@live.cn>
2026-01-21 22:15:06 -0800

    eqs find cover fix

--- Source/Chicago/Variant_Shooter/AI/EnvQueryContext_Target.cpp
+++ Source/Chicago/Variant_Shooter/AI/EnvQueryContext_Target.cpp
@@ -4,12 +4,12 @@
 #include "Variant_Shooter/AI/EnvQueryContext_Target.h"
 #include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
 #include "EnvironmentQuery/EnvQueryTypes.h"
-#include "ShooterAIController.h"
+#include "Character/AI/CHAIController.h"
 
 void UEnvQueryContext_Target::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
 {
 	// get the controller from the query instance
-	if (AShooterAIController* Controller = Cast<AShooterAIController>(QueryInstance.Owner))
+	if (ACHAIController* Controller = Cast<ACHAIController>(QueryInstance.Owner))
 	{
 		// ensure the target is valid
 		if (IsValid(Controller->GetCurrentTarget()))
